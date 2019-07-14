131ef05b796fc200f1222fcbedacefd0d5e2c102
Scott Wren <cynics0xff@gmail.com>
2019-07-13 23:21:37 -0400

    Add country details to html

--- src/app/lodge-profile/lodge-profile.component.ts
+++ src/app/lodge-profile/lodge-profile.component.ts
@@ -14,6 +14,7 @@ export class LodgeProfileComponent implements OnInit, ILodgeProfile {
   district: number;
 
   profileForm = new FormGroup({});
+  countries: string[] = ['USA', 'United Kingdom', 'Japan'];
   
   isLinear = false;
   lodgeFormGroup: FormGroup;
