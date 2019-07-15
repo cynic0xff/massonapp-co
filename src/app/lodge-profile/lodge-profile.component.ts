67756615236aa5ff43b47aa56b3b10cb67f076e9
Scott Wren <cynics0xff@gmail.com>
2019-07-14 21:28:14 -0400

    Add statement dinner and meeting field to html

--- src/app/lodge-profile/lodge-profile.component.ts
+++ src/app/lodge-profile/lodge-profile.component.ts
@@ -14,7 +14,12 @@ export class LodgeProfileComponent implements OnInit, ILodgeProfile {
   district: number;
 
   profileForm = new FormGroup({});
+
+  //TODO: country
   countries: string[] = ['USA'];
+  //days: number[] = Array.from(Array(31), (x, index) => index + 1);
+  days: string[] = ['1st', '2nd', '3rd', '4th'];
+  times: string[] = ['16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30', '22:00'];
   
   isLinear = false;
   lodgeFormGroup: FormGroup;
@@ -26,5 +31,7 @@ export class LodgeProfileComponent implements OnInit, ILodgeProfile {
       lodgeNumber: ['', Validators.required],
       lodgeName: ['', Validators.required]
     });
+
+    console.log(this.days);
   }
 }
